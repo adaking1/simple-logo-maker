9c653682b52cb495a8b685eb23b418746b036fec
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-09-28 18:06:46 -0400

    added comments

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ This application is a simple logo maker. It can be very useful for freelance web
 Install this application by first installing node.js to your machine. Then, install npm, inquirer version 8.2.4, is-Color and jest from npm.
 
 ## Usage
+[Usage Demo](https://drive.google.com/file/d/1Ye8waATmOEVz-Uu8xNjdYpTMuF0av3cH/view)  
 Start this application by first making sure that your terminal is located in the folder that holds the index.js file for this project. Type "node index.js" into the terminal to run the app. You will then be asked prompts through inquirer. The first prompt asks you to enter the text for your logo using up to three characters. The next prompt asks what color you would like the text. Next, you will be asked to select a shape from a list of three options (circle, square, triangle). After you select one, then the last prompt will ask you what color you want that shape. After all the prompts are answered, a logo.svg file is created and added to the current folder, and in the terminal "Generated logo.svg" is console logged.
 
 ## Contributing
--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const {Circle, Square, Triangle} = require("./lib/shapes");
 const textSvg = require("./lib/text-svg");
 const isColor = require("is-color");
 
+// This function initializes the app
+// It holds the inquirer prompts and uses fs to write over logo.svg if it exists and creates it if it doesn't
 function init() {
     inquirer.prompt([
         {
             type: "input",
             name: "text",
             message: "Enter text (3 characters or less)",
+            // this makes sure the text is three characters or less. It does not proceed until a valid input is entered
             validate: (text) => {
                 return text.length<=3 || "Text must be 3 characters or less";
             }
             
         },
         {
+            // this "when" function makes it so this prompt is only asked if the previous prompt actually has input. It doesn't ask for the color of the text if there isn't any
             when: (input) => input.text.length !== 0,
             type: "input",
             name: "textColor",
             message: "What color do you want this text?",
+            // this takes the user's input and removes spaces if there are any.
+            // it also turns the input to lowercase if it wasn't already
             filter: (answer) => {
                 if (answer.includes(" ")) {
                     const newString = answer.replaceAll(" ", "");
@@ -29,6 +35,7 @@ function init() {
                     return answer.toLowerCase();
                 }
             },
+            // this uses is-color package from npm to verify that the filtered input is a valid css color
             validate: (color) => {
                 if (color.length > 0) {
                     return isColor(color) || "Please enter a valid color"
@@ -48,9 +55,7 @@ function init() {
             type: "input",
             name: "shapeColor",
             message: "What color do you want this shape?",
-            validate: (color) => {
-                return color.length > 0 || "Enter a color"
-            },
+            // this removes spaces from the users input and turns the string lowercase if it was not already
             filter: (answer) => {
                 if (answer.includes(" ")) {
                     const newString = answer.replaceAll(" ", "");
@@ -60,6 +65,7 @@ function init() {
                     return answer.toLowerCase();
                 }
             },
+            // this then validates that the filtered color is a valid CSS color using "is-color"
             validate: (color) => {
                 if (color.length > 0) {
                     return isColor(color) || "Please enter a valid color"
@@ -72,8 +78,10 @@ function init() {
     ])
     .then((data) => {
         let shape;
+        // the next two lines create an instance of the text class and sets the color of that text object
         const txt = new textSvg(data.text, data.shape);
         txt.setTextColor(data.textColor);
+        // this creates an instance of the specified shape object and sets that object's color
         switch(data.shape) {
         case "Circle":
             shape = new Circle();
@@ -87,6 +95,8 @@ function init() {
         }
         shape.setColor(data.shapeColor);
 
+        // this uses node's file system to write over a file or create a new file if it doesn't exist
+        // it uses the shape and text objects' render() method to return the svg code for shape and text
         fs.writeFile("logo.svg", `<svg 
         width="300"
         height="200"
@@ -94,9 +104,10 @@ function init() {
         ${shape.render()}
         ${txt.render()}
         </svg>`);
-
+        // this is a success message that is console logged if the app was successful
         console.log("Generated logo.svg");
     })
+    // this catches any errors that may come up
     .catch((error) => {
         if (error.isTtyError) {
           console.error(error);
@@ -108,8 +119,3 @@ function init() {
 }
 
 init();
-
-
-
-// add comments
-// make demo video and add to readme
